b0d59260319c7eea9cfe22d6da43fb28e9d4563d
Leonardo Daines <leonardodaines123@gmail.com>
2022-03-22 09:29:22 -0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+
+
+
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado")
+
+function calcular(){
+    console.log("Calculando...");
+
+    let adultos = inputAdultos.value;
+    let crincas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+    let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * crincas);
+    let qdtTotalCerveja = cervejaPP(duracao) * adultos;
+    let qdtTotalBebida = bebidaPP(duracao) * adultos + (bebidaPP(duracao) / 2 * crincas)
+
+    resultado.innerHTML = `<p>${qdtTotalCarne / 1000} kg de Carne</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qdtTotalCerveja / 355)} Latas de Cerveja</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qdtTotalBebida / 2000)} Garrafas de Bebida</p>`
+
+} 
+
+function carnePP(duracao){
+    if(duracao >= 6){
+        return 650;
+    }else{
+        return 400;
+    }
+}
+
+function cervejaPP(duracao){
+    if(duracao >= 6){
+        return 2000;
+    }else{
+        return 1200;
+    }
+}
+
+function bebidaPP(duracao){
+    if(duracao >= 6){
+        return 1500;
+    }else{
+        return 1000;
+    }
+}
